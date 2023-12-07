a6d081fea4ef358b7438f77a8405c87632e384a5
abdulbasit-dev <abdullbasit99salah@gmail.com>
2023-12-07 18:39:58 +0300

    refactor the code

--- src/Main.java
+++ src/Main.java
@@ -3,73 +3,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Mortgage Calculator Project
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("MORTGAGE CALCULATOR");
         System.out.println("___________________");
         System.out.println("Please enter the following information: ");
 
-        int principal = getPrincipe(scanner);
-        float annualInterestRate = getAnnualInterestRate(scanner);
-        byte period = getPeriod(scanner);
+        int principal = (int) readNumber("Principal ($1k - $1M): ", 1000, 1_000_000);
+        float annualInterestRate = (float) readNumber("Annual Interest Rate: ", 1, 30);
+        byte period = (byte) readNumber("Period (Years): ", 1, 30);
 
-
-        double monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-        int numberOfPayments = period * MONTHS_IN_YEAR;
-
-        double mortgage = calculateMortgage(principal,monthlyInterestRate, numberOfPayments);
-
+        double mortgage = calculateMortgage(principal,annualInterestRate, period);
         displayMortgage(mortgage);
     }
 
-    public static int getPrincipe(Scanner scanner){
-        int principal;
+    public static double readNumber(String prompt, int min, int max){
+        Scanner scanner = new Scanner(System.in);
+        double value;
 
         while(true){
-            System.out.print("Principal ($1k - $1M): ");
-            principal = scanner.nextInt();
-            if(principal >= 1000 && principal <= 1_000_000)
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if(value >= min && value <= max)
                 break;
-            System.out.println("Enter a number between 1,000 and 1,000,000");
+
+            String minFormatted = NumberFormat.getInstance().format(min);
+            String maxFormatted = NumberFormat.getInstance().format(max);
+            System.out.println("Enter a number between " + minFormatted + " and " + maxFormatted);
         }
 
-        return principal;
+        return value;
     }
 
-    public static float getAnnualInterestRate(Scanner scanner){
-        float annualInterestRate;
 
-        while(true){
-            System.out.print("Annual Interest Rate: ");
-            annualInterestRate = scanner.nextFloat();
-            if(annualInterestRate >= 1 && annualInterestRate <= 30)
-                break;
-            System.out.println("Enter a value greater than 0 and less than or equal to 30");
-        }
-
-        return annualInterestRate;
-    }
-
-    public static byte getPeriod(Scanner scanner){
-        byte period;
-
-        while(true){
-            System.out.print("Period (Years): ");
-            period = scanner.nextByte();
-            if(period >= 1 && period <= 30)
-                break;
-            System.out.println("Enter a value between 1 and 30");
-        }
-
-        return period;
-    }
-
-    public static double calculateMortgage(int principal, double monthlyInterestRate, int numberOfPayments){
+    public static double calculateMortgage(int principal, float annualInterestRate , byte period){
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
         double mortgage;
+
+        double monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        int  numberOfPayments = period * MONTHS_IN_YEAR;
 
         mortgage = principal
                 * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
@@ -79,7 +50,7 @@ public class Main {
     }
 
     public static void displayMortgage(double mortgage){
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + mortgageFormatted);
     }
 
